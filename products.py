09ee705fb998a640a04b7ab891c9654aed67ccf3
Evanderss <evanderramirez150@gmail.com>
2022-10-25 16:35:57 -0500

    Creando datos a consumir

---
+++ products.py
@@ -0,0 +1,6 @@
+"""In order to simulate a database, this file was created where we can use the information"""
+products = [
+    {"name": "laptop", "price": 900, "quantity": 5},
+    {"name": "mouse", "price": 50, "quantity": 20},
+    {"name": "keyboard", "price": 200, "quantity": 14}
+]
