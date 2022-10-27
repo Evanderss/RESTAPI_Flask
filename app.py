7d5b15d5a0644b172032b52b3665c17d50a76a5b
Evanderss <evanderramirez150@gmail.com>
2022-10-26 23:29:43 -0500

    Agregando la ruta para productos y mostrandola

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ app = Flask(__name__)
 
 from products import products
 
-@app.route("/ping")
+#Testing route
+@app.route("/ping", methods=["GET"])
 def ping():
     return jsonify({"message": "Pong!"})
+
+#Getting data
+@app.route("/products", methods=["GET"])
+def getProduct():
+    return jsonify({"products": products, "message":"Products List"})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
